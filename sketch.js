a5df1bd15801b55107c56f3111cc522d77580427
Julia0p <jnicacio315@gmail.com>
2024-08-29 18:04:44 -0300

    salv

---
+++ sketch.js
@@ -0,0 +1,12 @@
+function setup() {
+    createCanvas(600, 400);
+    background('red');
+  }
+  
+  function draw() {
+    stroke('#0A0A0A')
+    fill('#0EEAD6');
+    //console.log(moseIsPressed);
+    if  (mouseIsPressed);   
+    rect( mouseX,mouseY, 30, 20);
+  }
